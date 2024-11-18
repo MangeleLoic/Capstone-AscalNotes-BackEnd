1ac394c3506cce7be7cee7b31e1f2633ebf1dae1
MangeleLoic <behonloic@gmail.com>
2024-11-18 07:50:58 +0100

    initial commit

---
+++ src/main/java/loicmangele/asclanotes/AsclanotesApplication.java
@@ -0,0 +1,13 @@
+package loicmangele.asclanotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsclanotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsclanotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loicmangele/asclanotes/AsclanotesApplicationTests.java
@@ -0,0 +1,13 @@
+package loicmangele.asclanotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsclanotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
