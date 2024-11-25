c78d9a8ae5e0390a330482edcf5ce8a414ab2bd4
MangeleLoic <behonloic@gmail.com>
2024-11-25 16:10:18 +0100

    adds securepassword

--- src/main/java/loicmangele/asclanotes/utente/AuthService.java
+++ src/main/java/loicmangele/asclanotes/utente/AuthService.java
@@ -3,6 +3,7 @@ package loicmangele.asclanotes.utente;
 import loicmangele.asclanotes.exceptions.UnauthorizedException;
 import loicmangele.asclanotes.tools.JWT;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,13 @@ public class AuthService {
     private UtenteService utenteService;
 
     @Autowired
+    private PasswordEncoder bcrypt;
+    @Autowired
     private JWT jwt;
     public String checkCredentials(UtenteLoginDTO body) {
 
         Utente found = this.utenteService.findByUsername(body.username());
-        if (found.getPassword().equals(body.password())) {
+        if (bcrypt.matches(body.password(), found.getPassword())) {
             String accessToken = jwt.createToken(found);
             return  accessToken;
         } else {
