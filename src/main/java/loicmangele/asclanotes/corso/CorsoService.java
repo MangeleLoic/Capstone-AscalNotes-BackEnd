b970fbb7fb4babb1963fd4554a1f05d0bac0c055
MangeleLoic <behonloic@gmail.com>
2024-11-26 15:08:40 +0100

    partial appunti/corsi

--- src/main/java/loicmangele/asclanotes/corso/CorsoService.java
+++ src/main/java/loicmangele/asclanotes/corso/CorsoService.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class CorsoService {
     @Autowired
@@ -39,12 +37,34 @@ public class CorsoService {
         return corsoRepository.findAll(pageable);
     }
 
-    public Optional<Corso> findById(Long id){
-        return this.corsoRepository.findById(id);
+    public Corso findById(Long id){
+        return this.corsoRepository.findById(id).orElseThrow(()-> new CorsoNotFoundException(id));
     }
+
+    public Corso findByName(String nome) {
+        return corsoRepository.findByNome(nome)
+                .orElseThrow(() -> new CorsoNotFoundByNameException(nome));
+    }
+
+    public Corso findByCode(String codice) {
+        return corsoRepository.findByCodice(codice)
+                .orElseThrow(() -> new CorsoNotFoundByCodiceException(codice));
+    }
+
 
     public Corso updateCorso(long id, CorsoDTO body) {
         return corsoRepository.findById(id).map(corso -> {
+            corsoRepository.findByNome(body.nome()).ifPresent(existingCorso -> {
+                if (!existingCorso.getId().equals(id)) {
+                    throw new BadRequestException("Un altro corso con il nome " + body.nome() + " è già presente.");
+                }
+            });
+
+            corsoRepository.findByCodice(body.codice()).ifPresent(existingCorso -> {
+                if (!existingCorso.getId().equals(id)) {
+                    throw new BadRequestException("Un altro corso con il codice " + body.codice() + " è già presente.");
+                }
+            });
             corso.setNome(body.nome());
             corso.setCodice(body.codice());
             corso.setDescrizione(body.descrizione());
