c4c5ba26ea4e838593b14420fbc843c275501865
MangeleLoic <behonloic@gmail.com>
2024-11-26 11:54:00 +0100

    partial appunti

--- src/main/java/loicmangele/asclanotes/appunto/AppuntoController.java
+++ src/main/java/loicmangele/asclanotes/appunto/AppuntoController.java
@@ -1,4 +1,71 @@
 package loicmangele.asclanotes.appunto;
 
+import jakarta.validation.Valid;
+import loicmangele.asclanotes.exceptions.AppuntoNotFoundException;
+import loicmangele.asclanotes.exceptions.BadRequestException;
+import loicmangele.asclanotes.utente.Utente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/appunti")
 public class AppuntoController {
+
+    @Autowired
+    private AppuntoService appuntoService;
+
+    @PostMapping
+    public Appunto creaAppunto(@Valid @RequestBody AppuntoDTO appuntoDTO,
+                                 @AuthenticationPrincipal Utente utenteLoggato) {
+        return appuntoService.saveAppunto(appuntoDTO, utenteLoggato);
+    }
+
+    //  GET http://localhost:3001/appunti
+    @GetMapping
+    public Page<Appunto> findAllAppunti(@RequestParam(defaultValue = "1") int page,
+                                @RequestParam(defaultValue = "10") int size,
+                                @RequestParam(defaultValue = "id") String sortBy) {
+        return this.appuntoService.findAllAppunti( page, size, sortBy);
+    }
+
+    //  GET http://localhost:3001/appunti/{appuntiId}
+    @GetMapping("/{appuntiId}")
+    public Appunto findById(@PathVariable Long appuntiId) {
+        return this.appuntoService.findById(appuntiId)
+                .orElseThrow(() -> new AppuntoNotFoundException(appuntiId));
+    }
+
+
+    //  PUT http://localhost:3001/appunti/{appuntoId} (+ req.body)
+    @PutMapping("/{appuntoId}")
+    public Appunto findByIdAndUpdate(@PathVariable Long appuntoId,
+                                    @RequestBody @Validated AppuntoDTO body,
+                                    BindingResult validationResult,
+                                     @AuthenticationPrincipal Utente utenteLoggato) {
+        if (validationResult.hasErrors()) {
+            String message = validationResult.getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload! " + message);
+        }
+        return this.appuntoService.updateAppunto(appuntoId, body, utenteLoggato);
+    }
+
+    //  DELETE http://localhost:3001/utenti/{utenteId} --> 204
+    @DeleteMapping("/{appuntoId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable Long appuntoId) {
+        this.appuntoService.findByIdAndDelete(appuntoId);
+    }
+
 }
--- src/main/java/loicmangele/asclanotes/appunto/AppuntoDTO.java
+++ src/main/java/loicmangele/asclanotes/appunto/AppuntoDTO.java
@@ -3,19 +3,14 @@ package loicmangele.asclanotes.appunto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public record AppuntoDTO(
-        @NotNull(message = "il titiolo è obbligatorio!")
+        @NotNull(message = "il titolo è obbligatorio!")
                 @Size(min = 5, max = 30, message = "il titolo dev'essere tra 5 e 30 caratteri!")
         String titolo,
         @NotNull(message = "il contenuto è obbligatorio!")
         String contenuto,
-        @NotNull(message = "la data di creazione dell'appunto è obbligatoria!")
-        LocalDateTime dataCreazione,
-        @NotNull(message = "LA data dell'ultima modifica dell'appunto è obbligatoria!")
-        LocalDateTime dataUltimaModifica,
         @NotNull(message = "l'utente che carica l'qppunto è obbligatorio!")
         Long utenteId,
         @NotNull(message = "Il corso associato all'appunto è obbligatorio!")
